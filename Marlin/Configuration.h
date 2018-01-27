3884f858f76aeb0183764861a4638f314647ef64
Pavel Suk <pavelsuk@users.noreply.github.com>
2018-01-27 10:37:18 +0100

    FIrst change of settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -32,7 +32,7 @@
 /**
  * Equipment options
  */
-//#define LARGE_BED
+#define LARGE_BED
 #define SDSUPPORT
 //#define CHANGE_Y_DIRECTION      // If your bed homes in the wrong direction (it should move front to back) enable this.
 //#define CHANGE_X_DIRECTION      // If your X carriage homes in the wrong direction (it should move right to left) enable this.
